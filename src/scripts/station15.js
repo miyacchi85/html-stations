de4689dbaff6c0dc7e1825d299a43d3d8cad2b69
miyacchi85 <miyahira@skyarch.net>
2024-03-05 06:14:05 +0900

    clear_station5

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -3,14 +3,34 @@ async function getData() {
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
   const userList = [
-    { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
-    { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
+    {
+      id: 1,
+      first_name: "優",
+      family_name: "大木",
+      affiliation: "TechTrain",
+      is_student: false,
+    },
+    {
+      id: 2,
+      first_name: "太郎",
+      family_name: "山田",
+      affiliation: "HogeHoge大学",
+      is_student: true,
+    },
   ];
-  const result = await test()
-  return await result
+  const result = await test(userList);
+  return await result;
 }
 
-function test() {
-  return
+function test(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      function buildFullName(item) {
+        item.full_name = item.family_name + " " + item.first_name;
+        return item;
+      }
+      const updateData = data.map(buildFullName);
+      resolve(updateData);
+    }, 3000);
+  });
 }
-
